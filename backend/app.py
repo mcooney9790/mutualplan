1425e2d98429e3001cd7503fee6d3526c3c87ae0
Myles <14080724+mcooney9790@users.noreply.github.com>
2024-11-27 08:00:17 -0800

    Update app.py allowing mutual plan frontend

--- backend/app.py
+++ backend/app.py
@@ -10,8 +10,7 @@ import os
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["*"])
-
+CORS(app, resources={r"/*": {"origins": "https://mutualplan.onrender.com"}})
 
 # Function to create calendar slide for each month
 def create_calendar_slide(prs, year, month, events):
